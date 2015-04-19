20d2c7ade2416aa626293d86430cf994f56e6117
vaio <orangeeeee.ore.ore@gmail.com>
2015-04-19 13:49:53 +0900

    second commit

---
+++ src/test/java/jp/co/skill/spider/SkillSpiderApplicationTests.java
@@ -0,0 +1,18 @@
+package jp.co.skill.spider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class SkillSpiderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
